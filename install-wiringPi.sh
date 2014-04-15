b65b950b987c034180047be8e711715b1f915754
Chris Continanza <christopher.continanza@gmail.com>
2014-04-14 19:06:16 -0700

    try by installing git

--- install-wiringPi.sh
+++ install-wiringPi.sh
@@ -1,5 +1,5 @@
-curl https://git.drogon.net/?p=wiringPi;a=snapshot;h=f18c8f7204d6354220fd6754578b3daa43734e1b;sf=tgz > wiringPi.tar.gz
-tar xfz wiringPi.tar.gz
+sudo apt-get install git-core
+git clone git://git.drogon.net/wiringPi
 cd wiringPi
+git pull origin
 ./build
-
